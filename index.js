c1c67f8d75c36b35befa76ebf371431245b0fe36
phytertek <ryan.phytertek@gmail.com>
2018-03-09 16:53:24 -0500

    add build server

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/'));
 
-app.listen(3000, error => {
+app.listen(process.env.port, error => {
   if (error) console.error(error);
-  console.log('Server is up');
+  console.log('Server is up on port ' + process.env.port);
 });
